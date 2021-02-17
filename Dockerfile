7f70b57943b05c20b6993e34977dc428d998012d
abdurrazaq <razaqofficial@gmail.com>
2021-02-17 14:33:57 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Stage 0, "build-stage", based on Node.js, to build and compile the frontend
+FROM node:alpine as build-stage
+
+# set up work directory
+WORKDIR '/app'
+
+# install dependency and build project
+COPY package*.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Stage 1, based on Nginx, to have only the compiled app, ready for production with Nginx
+FROM nginx
+COPY --from=build-stage /app/build /usr/share/nginx/html
+
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Welcome to my portfolio');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText('Meet Mazi AbdurRazaq');
   expect(linkElement).toBeInTheDocument();
 });
